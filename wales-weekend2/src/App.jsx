98fcdc21b36e479909462be892b6dcacf60fa5be
chezynick <chezynickannie@yahoo.co.uk>
2023-12-20 19:37:26 +0000

    ast check

--- wales-weekend2/src/App.jsx
+++ wales-weekend2/src/App.jsx
@@ -3,7 +3,7 @@ const App = ()=> {
   return (
     <div className="flex w-full justify-center">
      
-      <h1 className="text-red-600 text-5xl">Vite + React</h1>
+      <h1 className="text-red-600 text-5xl">Hi mummy!</h1>
     
     </div>
   )
