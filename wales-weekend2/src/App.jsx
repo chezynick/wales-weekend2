33287d21c0799213c9025b00cca3def93e528e64
chezynick <chezynickannie@yahoo.co.uk>
2023-12-21 11:23:58 +0000

    simplified

--- wales-weekend2/src/App.jsx
+++ wales-weekend2/src/App.jsx
@@ -9,7 +9,9 @@ import ThirdQuestion from './components/ThirdQuestion'
 
 const App = ()=> {
   const [picToShow, setPicToShow] = useState(0);
-
+  const moveAlong = () => {
+    setTimeout(()=>setPicToShow(picToShow + 1),500)
+  }
   return (
     <div className=" text-white w-full bg-gradient-to-b h-screen from-[#0d135c] via-[#2c348f] to-white ">
      <Snowfall />
@@ -18,29 +20,12 @@ const App = ()=> {
       <h1 className="text-white text-5xl">To Mum and Dad</h1>
       <div className=' font-serif text-6xl'>Merry Christmas</div>
     <div className="w-screen flex justify-center items-center">
-  <Present onClick={()=>{
-        setTimeout(()=>setPicToShow(1),1000);
-  }} showPresent={picToShow === 0}/> 
+  <Present onClick={()=> moveAlong()} showPresent={picToShow === 0}/> 
   <ImageContainer picNumber={picToShow} /></div>
   <Success showSuccess={picToShow === 4} />
-   {picToShow === 1 && ( <FirstQuestion onClick={()=>{
-  setTimeout(()=>
-    {
-      setPicToShow(2);
-    },1000)
-    }}/>)}
-       {picToShow === 2 && ( <SecondQuestion onClick={()=>{
-  setTimeout(()=>
-    {
-      setPicToShow(3);
-    },1000)
-    }}/>)}
-      {picToShow === 3 && ( <ThirdQuestion onClick={()=>{
-  setTimeout(()=>
-    {
-      setPicToShow(4);
-    },1000)
-    }}/>)}
+   {picToShow === 1 && ( <FirstQuestion onClick={()=> moveAlong()}/>)}
+       {picToShow === 2 && ( <SecondQuestion onClick={()=> moveAlong()}/>)}
+      {picToShow === 3 && ( <ThirdQuestion onClick={()=> moveAlong()}/>)}
       </div>
     </div>
   )
