151b6f97a5e726b13e98425d8cefcf71d0f5cd5c
chezynick <chezynickannie@yahoo.co.uk>
2023-12-21 14:22:26 +0000

    hide overflow

--- wales-weekend2/src/App.jsx
+++ wales-weekend2/src/App.jsx
@@ -17,7 +17,7 @@ const App = ()=> {
     },500)
   }
   return (
-    <div className=" text-white w-full bg-gradient-to-b h-screen from-[#0d135c] via-[#2c348f] to-white ">
+    <div className=" text-white w-full bg-gradient-to-b h-screen overflow-hidden from-[#0d135c] via-[#2c348f] to-white ">
      <Snowfall snowflakeCount={snowFlakes}/>
 
     <div className='w-full flex pt-12 flex-col justify-center items-center'>
