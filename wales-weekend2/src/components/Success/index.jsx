c19569d24ac86938adf881f90b9159a38997d523
chezynick <chezynickannie@yahoo.co.uk>
2023-12-21 11:45:28 +0000

    fixed hidden question bug

--- wales-weekend2/src/components/Success/index.jsx
+++ wales-weekend2/src/components/Success/index.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 const Success = ({showSuccess}) => {
 return (
-    <div className={`${showSuccess? 'w-full opacity-100' : 'w-0 opacity-0'} transition-all text-xl font-serif flex flex-col items-center justify-center delay-200`}>
+    <div className={`${showSuccess? 'w-full opacity-100' : 'w-0 h-0 opacity-0'} transition-all  text-xl font-serif flex flex-col items-center justify-center delay-200`}>
         <p>Pick a weekend, we will book a night or 2 in a 3 bed accommodation close to Ruthin, you will be chaffeur driven in a vintage ford fiesta.</p>
         <p>We can visit any locals you may know in the area, explore the glorious food and drink that wales has to offer (spoons? - other venues are available).</p>
         <p>Day 2 - beach / mock italian village / chippy the choice is yours!</p>
