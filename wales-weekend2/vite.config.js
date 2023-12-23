6feec3389d7be9247a9f0ae6211597be1f1b6c33
chezynick <chezynickannie@yahoo.co.uk>
2023-12-23 14:58:37 +0000

    updated named

--- wales-weekend2/vite.config.js
+++ wales-weekend2/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/wales-weekend2/",
+  base: "/xmas-present/",
   plugins: [react()],
 })
