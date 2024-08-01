a4659d1faa6ada1e2b599fb0903e7487b1e89695
chezynick <chezynickannie@yahoo.co.uk>
2024-08-01 14:36:23 +0100

    update to confirm

--- wales-weekend2/src/App.jsx
+++ wales-weekend2/src/App.jsx
@@ -8,34 +8,36 @@ import Success from './components/Success'
 import SleighBells from './assets/sleighBells.mp3'
 import useSound from 'use-sound';
 import ThirdQuestion from './components/ThirdQuestion'
+import ConfirmationPage from './components/ConfirmationPage'
 
 const App = ()=> {
-  const [picToShow, setPicToShow] = useState(0);
-  const [snowFlakes, setSnowFlakes] = useState(150)
-  const moveAlong = () => {
-    if(picToShow ===0)play()
-    setTimeout(()=>{
-      setPicToShow(picToShow + 1);
-      setSnowFlakes(snowFlakes + 300)
-    },300)
-  }
-  const [play] = useSound(SleighBells);
+  // const [picToShow, setPicToShow] = useState(0);
+  // const [snowFlakes, setSnowFlakes] = useState(150)
+  // const moveAlong = () => {
+  //   if(picToShow ===0)play()
+  //   setTimeout(()=>{
+  //     setPicToShow(picToShow + 1);
+  //     setSnowFlakes(snowFlakes + 300)
+  //   },300)
+  // }
+  // const [play] = useSound(SleighBells);
   return (
     <div className=" text-white w-full bg-gradient-to-b h-screen overflow-hidden from-[#0d135c] via-[#2c348f] to-white ">
-     <Snowfall snowflakeCount={snowFlakes}/>
+     <ConfirmationPage />
+     {/* <Snowfall snowflakeCount={snowFlakes}/>
   
-  <div className='w-full flex pt-12 flex-col justify-center items-center'>
+  <div className='flex flex-col items-center justify-center w-full pt-12'>
   {picToShow === 0 &&(  <div className="flex flex-col items-center">
-      <h1 className="text-white text-5xl">To Mum and Dad</h1>
-      <div className=' font-serif text-6xl'>Merry Christmas</div></div>)}
-    <div className="w-screen flex justify-center items-center">
+      <h1 className="text-5xl text-white">To Mum and Dad</h1>
+      <div className='font-serif text-6xl '>Merry Christmas</div></div>)}
+    <div className="flex items-center justify-center w-screen">
   <Present onClick={()=> moveAlong()} showPresent={picToShow === 0}/> 
   <ImageContainer picNumber={picToShow} /></div>
   <Success showSuccess={picToShow === 4} />
    {picToShow === 1 && ( <FirstQuestion onClick={()=> moveAlong()}/>)}
        {picToShow === 2 && ( <SecondQuestion onClick={()=> moveAlong()}/>)}
       {picToShow === 3 && ( <ThirdQuestion onClick={()=> moveAlong()}/>)}
-      </div>
+      </div> */}
     </div>
   )
 }
---
+++ wales-weekend2/src/components/ConfirmationPage/index.jsx
@@ -0,0 +1,14 @@
+const ConfirmationPage = () => {
+    return (
+        <div>
+            <h1>It's time to pack those bags (prob just the 1 bag...small boot on these Fiestas)</h1>
+            <div>Pick up will be between 9 and 9.30am - hangover permitting</div>
+            <div>You will be driven to deepest darkest Wales</div>
+            <div>Staying in your home away from home at the Memorial Building in your own 2 bed apartment</div>
+            <a href="https://www.memorialbuildingapartments.co.uk/" >Click here to view</a>
+            <div className="test-red animate-pulse">Smoking sets off the smoke alarms so outside only!! (I dont want to lose my deposit/your paying any fines!)</div>
+        </div>
+    );
+}
+
+export default ConfirmationPage;
